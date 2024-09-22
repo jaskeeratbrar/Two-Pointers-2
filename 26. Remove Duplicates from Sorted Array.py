ec47f8579e21fa00b1e8ea1e2c864f84cad46687
Jaskeerat Brar <jassibrar1999@gmail.com>
2024-09-21 19:12:51 -0700

    Create 26. Remove Duplicates from Sorted Array.py

---
+++ 26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,20 @@
+# T(c) - O(n)
+# S(c) - O(1)
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        l = 0 
+
+        for i in range(1, len(nums)):
+
+            if nums[l] != nums[i]:
+                l += 1
+                nums[l] = nums[i]
+            
+
+        return l + 1
+
+            
+
+        
